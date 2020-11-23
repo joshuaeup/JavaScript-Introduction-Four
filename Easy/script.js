1d2f43aea6a50507f619f1136e8696160c273801
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-11-23 11:39:21 -0500

    Added very-hard

--- Easy/script.js
+++ Easy/script.js
@@ -6,17 +6,11 @@ const input2 = [10, 5];
 const input3 = [1.5, 3, 2.5, 1];
 
 // Function to calculate array average
-const avg = (arr) => {
-    let sum = 0;
-    // Loop through entire array and generate sum of values
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    // Print average to console
-    console.log(sum / arr.length);
+const average = (arr) => {
+    return arr.reduce((a, b) => a + b, 0) / arr.length;
 };
 
 // Call function and pass in arrays
-avg(input1);
-avg(input2);
-avg(input3);
+console.log(average(input1));
+console.log(average(input2));
+console.log(average(input3));
--- Very-Hard/script.js
+++ Very-Hard/script.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const coins = [1, 2, 5];
+const goal = 11;
+
+// const coins = [2];
+// const goal = 11;
+
+function combination(coins, value) {
+    // Exit condition for recursion
+    if (value == 0) return 0;
+
+    // Set combinationAmount variable to the largest number possible in JS
+    let combinationAmount = Number.MAX_VALUE;
+
+    // Loop through the entire length of the coins array
+    for (let i = 0; i < coins.length; i++) {
+        // if the coin at index i is less than or equal to value -> continue
+        if (coins[i] <= value) {
+            // Call function on itself and pass in values and subtract the coin at index i from the goal value
+            let innerFunction = combination(coins, value - coins[i]);
+
+            // Check to insure the innerFunctions result does not exceed the maximum value allowed in JS
+            if (innerFunction === Number.MAX_VALUE) {
+                return -1;
+                // Checls of the innerFunction value is less than the current recored combinationAmount value on line 12
+            } else if (innerFunction + 1 < combinationAmount) {
+                // if the innerFunction is less then you will set the combinationAmount to the returned innerFunction value
+                combinationAmount = innerFunction + 1;
+            }
+        }
+    }
+
+    return combinationAmount;
+}
+
+console.log(combination(coins, goal));
